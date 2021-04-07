6665f45f04a7e0b125010971165f89e900312314
dmg59912 <dmg138380@yahoo.com>
2021-04-06 19:47:00 -0700

    done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+import math
 import h5py
 
 
@@ -18,81 +19,75 @@ if __name__ == '__main__':
         #save data into our array 
         data = np.array(dataset)
         print('Shape of data:\n', data.shape)
-      
-      
-        let = len(data) * len(data[0])
+    
 
-
-        #make a copy of data into trasfer size (943000,2) so that we dont modify data array 
-        transfer_arr = np.zeros((let,2))
-        #keep tract of our transfer array index with count2
-        count2 = 0
-        for i in range(len(data)):
-            for j in range(len(data[0])):
-
-                transfer_arr[count2][0] = data[i][j][0]
-                transfer_arr[count2][1] = data[i][j][1]
-                count2 +=1 
-        #find unique values in our data array and transfer array
-        vals, idxs = np.unique(data, return_index = True)
-        vals2, idxs = np.unique(data, return_index = True)
+        index_to_plot = [0,100,200,300,400,500,600,700,800,900]
         
-        # print(vals)
-        # print(vals2)
 
-        # if np.array_equal(vals,vals2):
-        #     print("values 1 is the same as values 2")
-
-        #where we are storing y mins and max values correspoinding to therr X values (X, y_min, y_max)
-        min_max = np.zeros((let,3))
-        
         print("\n\n\nand then ...")
 
-        #to add all our y values before findind min and max
 
-
-        # temp = []
-        # count = 0
-        # for i in range(len(vals)):
-        #     temp = []
-        #     for j in range(len(transfer_arr)):
-        #         if vals[i] == transfer_arr[j][0]:
-        #             #print(vals[i], ", x", transfer_arr[j][0], ",y", transfer_arr[j][1])
-        #             temp.append(transfer_arr[j][1])
-
-        #     if( len(temp)!= 0):
-               
-        #         ymin = min(temp)
-        #         ymax = max(temp)
-        #         #print('min', ymin, 'max', ymax)
-        #         min_max[count][0] = vals[i]
-        #         min_max [count][1] = ymin
-        #         min_max[count][2] = ymax
-        #         count += 1
-
-
-       
-
-        # #removing zeros from the array 
-        # min_max = min_max[~np.all(min_max ==0, axis= 1)]
-        # print(min_max)
-
-
-        x,y = transfer_arr[:,0], transfer_arr[:,1]
-        r = np.sqrt(x **2, y** 2 )
-        phi = np.arctan(y, x)
-
-        print(min_max.shape)
-        plt.title("Polar transformation of boundary points")
-        plt.axes( projection = 'polar')
-        plt.polar(phi,r, 'b.')
-        plt.show()
         
-        #print(transfer_arr)
-       # print(len(transfer_arr))
-        #print(vals)
-        #print(sum(arr))
-        #print(min_max.shape)
-        #print(len(vals))
-        #print(data)
+        for k in range(len(index_to_plot)):
+
+            #find unique values in our data array according to our index
+            index_t = index_to_plot[k]
+            print("printing index ",index_t)
+            vals3 = np.unique(data[index_t, :, :1])
+
+            #creating a temp array to store our y values from the unique x's on the given index
+            temp = []
+
+            #where we are storing y mins and max values correspoinding to therr X values (X, y_min, y_max)
+            min_max = np.zeros((len(vals3),3))
+            count = 0
+            for i in range(len(vals3)):
+                temp = []
+                for j in range(len(data[k])):
+                    if vals3[i] == data[k][j][0]:
+                        temp.append(data[k][j][1])
+                if( len(temp)!= 0):
+                    ymin = min(temp)
+                    ymax = max(temp)
+                    min_max[count][0] = vals3[i]
+                    min_max[count][1]= ymin
+                    min_max[count][2] = ymax
+                    count += 1
+        
+        
+            #removing zeros from the array 
+          
+            #fill our x and y values so that we can calculate r for x ymin and x ymax
+            fill = len(min_max) * 2 
+            x = [0] * fill
+            y = [0] * fill
+          
+          
+            count3 = 0
+            for i in range(len(min_max)):
+                for j in range(2):
+                    x[count3] =  min_max[i][0]
+                    y[count3] = min_max[i][j + 1]
+                    # sum_sq = x[count3] **2 + y[count3] ** 2
+                    # r[count3] = np.sqrt(sum_sq)
+                    count3 += 1
+
+
+    
+
+            #convert to array instead of list
+            x_p = np.array(x)
+            y_p = np.array(y)
+        
+            #finding all the Rs 
+            r = np.sqrt(x_p **2 + y_p ** 2)
+            #finding thats 
+            thetas = np.arctan2(y_p, x_p) 
+
+            #plotting the graphs 
+            plt.scatter(thetas,r)
+            plt.title("Plot for index " + str(index_to_plot[k]), va = 'bottom')
+            plt.show()
+            plt.savefig("pic " + str(k), format ='eps', dpi=1000)
+            plt.close()
         
